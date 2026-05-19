6a22b4d1136182602cb240e43baa33f154c7779c
SaidCyre <69218719+saidcyre@users.noreply.github.com>
2026-05-18 21:58:22 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'electrocyre-v1';
+
+// Arquivos que ficam disponíveis offline
+const PRECACHE = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(PRECACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  // Remove caches antigos
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Estratégia: network-first (tenta internet, cai no cache se offline)
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        // Salva cópia no cache
+        if (res && res.status === 200 && e.request.method === 'GET') {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
